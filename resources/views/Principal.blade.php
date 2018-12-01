0900396c0382f57c3972bd34ed11fc6c05762dd9
Gerbi <gerbi@protonmail.com>
2018-11-30 23:14:14 -0700

    Create page main

---
+++ resources/views/Principal.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <!-- import CSS -->
+    {{--<link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">--}}
+
+</head>
+<body style="background-color: #fff;">
+<div id="app">
+    <app></app>
+</div>
+
+<script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
