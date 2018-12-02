9f179106695f0b99bf20a036ae00e992ee3ec36f
Gerbi <gerbi@protonmail.com>
2018-12-01 18:17:30 -0700

    remove comm

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,6 @@
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import axios from 'axios'
 
 Vue.use(VueRouter)
 
